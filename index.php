d59c9aa958eb756c129fb7c4d22e91c5c4496ac9
Shriya S Samak <sss329@afsconnect1.njit.edu>
2017-10-21 16:17:49 -0400

    added main class

--- index.php
+++ index.php
@@ -1,5 +1,90 @@
 <?php
 
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+
+class Manage
+
+{
+
+public static function autoload($class)
+
+{
+
+include $class .'.php';
+
+}
+
+}
+
+spl_autoload_register(array('Manage','autoload'));
+
+$obj= new main();
+
+class main 
+
+{
+
+public function _construct()
+
+{
+
+$pageRequest = 'homepage';
+
+if (isset($_REQUEST['page']))
+
+{
+
+$pageRequest = $_REQUEST['page'];
+
+}
+
+$page = new $pageRequest;
+
+if ($_SERVER['REQUEST_METHOD']=='GET)
+
+{
+
+$page->get();
+
+}
+
+else
+
+{
+$page->post();
+
+}
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
